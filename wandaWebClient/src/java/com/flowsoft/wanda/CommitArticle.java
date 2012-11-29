02bbcb4dd0a5c77bf624d70d6cba5772b5bb490d
andika.nagy.c <andika.nagy.c@a8fb0568-321d-44c5-52bf-e798346062f5>
2012-11-29 16:55:24 +0000

    New webservices

--- wandaWebClient/src/java/com/flowsoft/wanda/CommitArticle.java
+++ wandaWebClient/src/java/com/flowsoft/wanda/CommitArticle.java
@@ -8,18 +8,18 @@ import com.flowsoft.domain.Article;
 
 /**
  * <p>
- * Java class for findArticleByTitleResponse complex type.
+ * Java class for commitArticle complex type.
  * 
  * <p>
  * The following schema fragment specifies the expected content contained within
  * this class.
  * 
  * <pre>
- * &lt;complexType name="findArticleByTitleResponse">
+ * &lt;complexType name="commitArticle">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="article" type="{http://wanda.flowsoft.com/}article" minOccurs="0"/>
+ *         &lt;element name="arg0" type="{http://wanda.flowsoft.com/}article" minOccurs="0"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -29,30 +29,30 @@ import com.flowsoft.domain.Article;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "findArticleByTitleResponse", propOrder = { "article" })
-public class FindArticleByTitleResponse {
+@XmlType(name = "commitArticle", propOrder = { "arg0" })
+public class CommitArticle {
 
-	protected Article article;
+	protected Article arg0;
 
 	/**
-	 * Gets the value of the article property.
+	 * Gets the value of the arg0 property.
 	 * 
 	 * @return possible object is {@link Article }
 	 * 
 	 */
-	public Article getArticle() {
-		return article;
+	public Article getArg0() {
+		return arg0;
 	}
 
 	/**
-	 * Sets the value of the article property.
+	 * Sets the value of the arg0 property.
 	 * 
 	 * @param value
 	 *            allowed object is {@link Article }
 	 * 
 	 */
-	public void setArticle(Article value) {
-		this.article = value;
+	public void setArg0(Article value) {
+		this.arg0 = value;
 	}
 
 }
---
+++ wandaWebClient/src/java/com/flowsoft/wanda/FindCategoryByName.java
@@ -0,0 +1,60 @@
+
+package com.flowsoft.wanda;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for findCategoryByName complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="findCategoryByName">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="categoryName" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "findCategoryByName", propOrder = {
+    "categoryName"
+})
+public class FindCategoryByName {
+
+    protected String categoryName;
+
+    /**
+     * Gets the value of the categoryName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    /**
+     * Sets the value of the categoryName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCategoryName(String value) {
+        this.categoryName = value;
+    }
+
+}
