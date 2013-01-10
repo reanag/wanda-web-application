dc1e5dbe9094d36bb440c22c523850bd5cae8dbd
andika.nagy.c <andika.nagy.c@a8fb0568-321d-44c5-52bf-e798346062f5>
2013-01-10 10:23:02 +0000

    userdetails service & implementation after refactor

--- WandaWebapp/src/java/com/flowsoft/wanda/UserDetailsService.java
+++ WandaWebapp/src/java/com/flowsoft/wanda/UserDetailsService.java
@@ -10,58 +10,51 @@ import com.flowsoft.domain.Article;
 import com.flowsoft.domain.ArticleHeader;
 import com.flowsoft.domain.Category;
 import com.flowsoft.domain.Comment;
+import com.flowsoft.domain.Tag;
 import com.flowsoft.domain.WandaUser;
 
 @WebService
+// @SOAPBinding(style = Style.DOCUMENT, use = Use.ENCODED)
 public interface UserDetailsService {
 
-	void createArticle(@WebParam(name = "owner") WandaUser owner,
-			@WebParam(name = "categoryName") String category,
-			@WebParam(name = "articleTitle") String title,
-			@WebParam(name = "articleContent") String content,
-			@WebParam(name = "taglist") String[] taglist);
-
-	void commitArticle(Article a);
-
-	Category findCategoryByName(@WebParam(name = "categoryName") String name);
-
-	@WebResult(name = "categoryList")
-	List<Category> findAllExistingCategory();
-
-	//
 	@WebResult(name = "exist")
 	Boolean exist(@WebParam(name = "username") String username);
 
+	@WebResult(name = "wandaUserList")
+	List<WandaUser> findAllUser();
+
 	@WebResult(name = "wandaUser")
-	WandaUser findUserByUsername(@WebParam(name = "username") String username);
+	WandaUser findByUsername(@WebParam(name = "username") String username);
 
 	@WebResult(name = "articleList")
-	List<Article> findAllArticleByUsername(
-			@WebParam(name = "username") String username);
-
-	@WebResult(name = "articleHeaderList")
-	List<ArticleHeader> findAllArticleHeader();
+	List<Article> findAllArticle(@WebParam(name = "username") String username);
 
 	@WebResult(name = "article")
-	com.flowsoft.domain.Article findArticleByTitle(
+	Article findArticleByTitle(@WebParam(name = "articleTitle") String title);
+
+	@WebResult(name = "articleContent")
+	String getArticleContentByTitle(
 			@WebParam(name = "articleTitle") String title);
 
-	@WebResult(name = "commentList")
-	List<Comment> findAllCommentFor(
-			@WebParam(name = "articleTitle") String articleTitle);
+	// @WebResult(name = "commentList")
+	// List<Comment> findAllCommentFor(
+	// @WebParam(name = "articleTitle") String articleTitle);
 
-	@WebResult(name = "userCreationSuccess")
+	@WebResult(name = "creationSuccess")
 	Boolean createUser(@WebParam(name = "username") String username,
 			@WebParam(name = "password") String password,
 			@WebParam(name = "firstName") String firstName,
 			@WebParam(name = "lastName") String lastName);
 
-	@WebResult(name = "categoryCreationSuccess")
-	Boolean createCategory(@WebParam(name = "wandaUser") WandaUser owner,
+	void createCategory(@WebParam(name = "wandaUser") WandaUser owner,
 			@WebParam(name = "categoryName") String name);
 
 	void deleteCategory(@WebParam(name = "categoryName") String name,
 			@WebParam(name = "aktUsername") String aktuser);
+
+	void createArticle(@WebParam(name = "wandaUser") WandaUser owner,
+			@WebParam(name = "articleTitle") String title,
+			@WebParam(name = "articleContent") String content);
 
 	String deleteArticle(@WebParam(name = "articleTitle") String title,
 			@WebParam(name = "aktUser") String aktuser);
@@ -69,4 +62,28 @@ public interface UserDetailsService {
 	void editArticle(@WebParam(name = "articleTitle") String title,
 			@WebParam(name = "newContent") String newContent);
 
+	@WebResult(name = "tagList")
+	public List<Tag> getAllTag();
+
+	@WebResult(name = "categoryList")
+	public List<Category> findAllExistingCategory();
+
+	public void commitArticle(@WebParam(name = "article") Article a);
+
+	@WebResult(name = "userCategoryList")
+	public List<Category> getUserCategories(
+			@WebParam(name = "userid") Integer userid);
+
+	@WebResult(name = "article")
+	public Article findArticleByHeader(
+			@WebParam(name = "headerId") Integer headerId);
+
+	@WebResult(name = "commentList")
+	public List<Comment> findAllCommentFor(
+			@WebParam(name = "articleId") Integer articleId);
+
+	@WebResult(name = "articleHeaderList")
+	public List<ArticleHeader> findAllArticleHeader();
+
+	public void commitComment(@WebParam(name = "comment") Comment c);
 }
--- WandaWebapp/src/java/com/flowsoft/wanda/UserDetailsServiceImpl.java
+++ WandaWebapp/src/java/com/flowsoft/wanda/UserDetailsServiceImpl.java
@@ -2,7 +2,6 @@ package com.flowsoft.wanda;
 
 import java.util.List;
 
-import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
@@ -14,6 +13,7 @@ import com.flowsoft.domain.Article;
 import com.flowsoft.domain.ArticleHeader;
 import com.flowsoft.domain.Category;
 import com.flowsoft.domain.Comment;
+import com.flowsoft.domain.Tag;
 import com.flowsoft.domain.WandaUser;
 
 @WebService(name = "userDetailsService", targetNamespace = "http://com.flowsoft.wanda.UserDetailsServiceImpl", endpointInterface = "com.flowsoft.wanda.UserDetailsService")
@@ -48,9 +48,24 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	}
 
 	@Override
-	public List<Comment> findAllCommentFor(String articleTitle) {
-		return articleDao.findAllCommentFor(articleTitle);
+	public List<WandaUser> findAllUser() {
+		return userDao.findAllUser();
 	}
+
+	@Override
+	public WandaUser findByUsername(String username) {
+		return userDao.findUserByName(username);
+	}
+
+	@Override
+	public List<Article> findAllArticle(String username) {
+		return articleDao.findAllArticle(username);
+	}
+
+	// @Override
+	// public List<Comment> findAllCommentFor(String articleTitle) {
+	// return articleDao.findAllCommentFor(articleTitle);
+	// }
 
 	@Override
 	public Boolean createUser(String username, String password,
@@ -63,87 +78,84 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	}
 
 	@Override
-	public com.flowsoft.domain.Article findArticleByTitle(String title) {
-		return articleDao.getArticleByTitle(title);
+	public Article findArticleByTitle(String title) {
+		return articleDao.findArticleByTitle(title);
+	}
+
+	@Override
+	public String getArticleContentByTitle(String title) {
+		return articleDao.getArticleContentByTitle(title);
+	}
+
+	@Override
+	public void createCategory(WandaUser owner, String name) {
+		articleDao.createCategory(owner, name);
+
 	}
 
 	@Override
 	public void deleteCategory(String name, String aktuser) {
-		// articleDao.createCategory(name, aktuser);
+		articleDao.deleteCategory(name, aktuser);
 	}
 
 	@Override
-	public void createArticle(WandaUser owner, String category, String title,
-			String content, String[] taglist) {
-
-		articleDao.createArticle(owner, category, title, content, taglist);
-		logger.debug("create article with title: " + title);
-	}
-
-	@Override
-	public void commitArticle(Article a) {
-		articleDao.createArticle(a);
-		logger.debug("create article from domain object with title: "
-				+ a.getTitle());
+	public void createArticle(WandaUser owner, String title, String content) {
+		articleDao.createArticle(owner, title, content);
 	}
 
 	@Override
 	public String deleteArticle(String title, String aktUsername) {
-		return "d";
-		// return articleDao.deleteArticle(title, aktUsername);
+		return articleDao.deleteArticle(title, aktUsername);
 
 	}
 
 	@Override
 	public void editArticle(String title, String newContent) {
-		// articleDao.editArticle(title, newContent);
+		articleDao.editArticle(title, newContent);
 
 	}
 
 	@Override
-	@WebResult(name = "articleHeaderList")
-	public List<ArticleHeader> findAllArticleHeader() {
-		return articleDao.listArticleHeaders();
+	@WebResult(name = "tagList")
+	public List<Tag> getAllTag() {
+		return articleDao.getListTags();
 	}
 
 	@Override
-	@WebResult(name = "wandaUser")
-	public WandaUser findUserByUsername(
-			@WebParam(name = "username") String username) {
-		// TODO Auto-generated method stub
-		return userDao.findUserByName(username).convertToDomain();
-	}
-
-	@Override
-	@WebResult(name = "articleList")
-	public List<Article> findAllArticleByUsername(
-			@WebParam(name = "username") String username) {
-		return articleDao.findAllArticle(username);
-	}
-
-	@Override
-	@WebResult(name = "categoryCreationSuccess")
-	public Boolean createCategory(
-			@WebParam(name = "wandaUser") WandaUser owner,
-			@WebParam(name = "categoryName") String name) {
-		// TODO Auto-generated method stub
-
-		return true;
-	}
-
-	@Override
-	@WebResult(name = "category")
-	public Category findCategoryByName(
-			@WebParam(name = "categoryName") String name) {
-		// TODO Auto-generated method stub
-		return articleDao.findCategoryByName(name).convertToDomain();
-	}
-
-	@Override
-	@WebResult(name = "categoryList")
 	public List<Category> findAllExistingCategory() {
-		// TODO Auto-generated method stub
-		return articleDao.findAllExistingCategory();
+		return articleDao.findAllCategory();
 	}
 
+	@Override
+	public void commitArticle(Article a) {
+		articleDao.persistArticle(a);
+
+	}
+
+	@Override
+	public List<Category> getUserCategories(Integer userid) {
+		return articleDao.findCategoryBy(userid);
+	}
+
+	@Override
+	public Article findArticleByHeader(Integer headerId) {
+		return articleDao.findArticleById(headerId);
+	}
+
+	@Override
+	public List<Comment> findAllCommentFor(Integer articleId) {
+		return articleDao.findAllCommentFor(articleId);
+	}
+
+	@Override
+	public List<ArticleHeader> findAllArticleHeader() {
+		return articleDao.findAllArticle();
+
+	}
+
+	@Override
+	public void commitComment(Comment c) {
+		articleDao.persistComment(c);
+
+	}
 }
