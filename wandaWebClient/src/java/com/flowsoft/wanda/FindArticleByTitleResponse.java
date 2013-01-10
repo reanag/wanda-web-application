c7157605fe75c5f3d9ec35f607025a68a3b4c4b5
andika.nagy.c <andika.nagy.c@a8fb0568-321d-44c5-52bf-e798346062f5>
2013-01-10 10:00:56 +0000

    webservice files

---
+++ wandaWebClient/src/java/com/flowsoft/wanda/FindArticleByTitleResponse.java
@@ -0,0 +1,62 @@
+
+package com.flowsoft.wanda;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+import com.flowsoft.domain.Article;
+
+
+/**
+ * <p>Java class for findArticleByTitleResponse complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="findArticleByTitleResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="article" type="{http://wanda.flowsoft.com/}article" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "findArticleByTitleResponse", propOrder = {
+    "article"
+})
+public class FindArticleByTitleResponse {
+
+    protected Article article;
+
+    /**
+     * Gets the value of the article property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Article }
+     *     
+     */
+    public Article getArticle() {
+        return article;
+    }
+
+    /**
+     * Sets the value of the article property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Article }
+     *     
+     */
+    public void setArticle(Article value) {
+        this.article = value;
+    }
+
+}
